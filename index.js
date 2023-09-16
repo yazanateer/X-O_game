0d767fe0517ffc360d1e0caa5f72c1628d06130c
unknown <yazan.ateerr@gmail.com>
2023-09-16 13:19:41 +0300

    created and designed the board, added squares

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const board = document.querySelector('#board')
+
+const squares = new Array(9).fill("");
+
+(function(){
+    squares.forEach((cell, i) => {
+        const elements = document.createElement('div')
+        elements.classList.add('square')
+        elements.id = i;
+        board.append(elements)
+    }
+    )
+})();
