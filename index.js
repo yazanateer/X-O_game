9fe4b960f9c7a8440e1de68bd7dbb8a2dc1fe910
unknown <yazan.ateerr@gmail.com>
2023-09-16 14:51:04 +0300

    almost done, only to make the clikc on the on square twice not print more than one time

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const board = document.querySelector('#board')
-
 const squares = new Array(9).fill("");
+
+let current_player = "X";
 
 (function(){
     squares.forEach((cell, i) => {
         const elements = document.createElement('div')
         elements.classList.add('square')
         elements.id = i;
+        elements.addEventListener("click", print_player)
         board.append(elements)
     }
     )
 })();
+
+function print_player(squ){
+    const view_player = document.createElement('div')
+    view_player.classList.add(current_player)
+    view_player.textContent = current_player
+    squ.target.append(view_player)
+    current_player = (current_player=== "X"?"O":"X")
+
+}
